a4c88c0ef0a45a590fd9d508efae4790c487792a
Andre <als260602@msn.com>
2021-04-08 09:02:06 -0300

    enviando para forum da rocketseat

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,7 @@
+import { useSession, signIn } from 'next-auth/client';
+import { useCallback } from 'react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,10 +9,35 @@ interface SubscribeButtonProps {
 }
 
 export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const [session] = useSession();
+
+  const handleSubscribe = useCallback(async () => {
+    if (!session) {
+      await signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId })
+
+    } catch (error) {
+      alert(error.message)
+      console.log(error)
+    }
+
+  }, [session])
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import { query as q } from 'faunadb'
 
 import NextAuth from 'next-auth'
+import { session } from 'next-auth/client'
 import Providers from 'next-auth/providers'
 import { fauna } from '../../../services/fauna'
 
@@ -13,18 +14,26 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    // async session(sessions) {
+    //   const userActiveSubscriptions = await fauna.query(
+    //     q.Get(
+
+    //     )
+    //   )
+
+    //   return session
+    // },
     async signIn(user, account, profile) {
       const { email } = user
-
-      console.log(user);
-
       try {
         await fauna.query(
           q.If(
             q.Not(
-              q.Match(
-                q.Index('user_by_email'),
-                q.Casefold(email)
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
               )
             ),
             q.Create(
@@ -43,6 +52,7 @@ export default NextAuth({
         return true;
 
       } catch (error) {
+        console.log(error)
         return false
       }
 
